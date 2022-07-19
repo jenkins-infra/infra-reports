1f41669ee34c386f02538bd3e22dd647db6a0455
Gavin Mogan <git@gavinmogan.com>
2022-07-18 19:41:17 -0700

    Use builder (closes #42)

--- maintainers-info-report/maintainers-info-report.sh
+++ maintainers-info-report/maintainers-info-report.sh
@@ -4,16 +4,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-## Ensure that jq v1.5 (v1.6 and alpine v1.5 are not working as expected)
-wget -q -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 || { echo "Failed to download jq" >&2 ; exit 1 ; }
-chmod +x jq || { echo "Failed to make jq executable" >&2 ; exit 1 ; }
-
 LIST_URL='https://reports.jenkins.io/maintainers.index.json'
 echo "Querying URL: $LIST_URL" >&2
 curl --silent --fail -u "$JIRA_AUTH" "$LIST_URL" > maintainers.index.json
 
 declare -a MAINTAINERS_LIST
-output="$( ./jq --raw-output '.[][]' maintainers.index.json | sort -u )" # Despite INFRA-2924 we can use raw because maintainers cannot have spaces per RPU
+output="$( jq --raw-output '.[][]' maintainers.index.json | sort -u )" # Despite INFRA-2924 we can use raw because maintainers cannot have spaces per RPU
 readarray -t MAINTAINERS_LIST <<< "$output"
 
 echo '' > jira-users-list-tmp.json # clear file from any previous executions
@@ -23,7 +19,7 @@ for USERNAME in "${MAINTAINERS_LIST[@]}" ; do
   echo "Querying URL: $URL" >&2
   curl --silent --fail -u "$JIRA_AUTH" "$URL" > jira-user-tmp.json
 
-  ./jq '{ name, displayName }' jira-user-tmp.json >> jira-users-list-tmp.json
+  jq '{ name, displayName }' jira-user-tmp.json >> jira-users-list-tmp.json
 done
 
-./jq --slurp '.' jira-users-list-tmp.json
+jq --slurp '.' jira-users-list-tmp.json
