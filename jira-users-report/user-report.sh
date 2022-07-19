1edaafee4fd1f01abd94a1852e2150d7f1adff11
Gavin Mogan <git@gavinmogan.com>
2022-07-19 13:22:38 -0700

    Use builder (#43)
    * Use builder (closes #42)
    
    * Package management and stuff for ruby scripts

--- jira-users-report/user-report.sh
+++ jira-users-report/user-report.sh
@@ -4,10 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-## Ensure that jq v1.5 (v1.6 and alpine v1.5 are not working as expected)
-wget -q -O jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 || { echo "Failed to download jq" >&2 ; exit 1 ; }
-chmod +x jq || { echo "Failed to make jq executable" >&2 ; exit 1 ; }
-
 declare -a ALL_USERS
 
 URL="https://issues.jenkins.io/rest/api/2/group/member?groupname=jira-users"
@@ -15,18 +11,18 @@ while true ; do
   echo "Querying URL: $URL" >&2
   curl --silent --fail -u "$JIRA_AUTH" "$URL" > jira-users-tmp.json
 
-  output="$( ./jq '.values[] | .name' jira-users-tmp.json )" # Deliberately not raw because INFRA-2924
+  output="$( jq '.values[] | .name' jira-users-tmp.json )" # Deliberately not raw because INFRA-2924
   readarray -t USERS <<< "$output"
 
   ALL_USERS+=( "${USERS[@]}" )
 
-  done="$( ./jq --raw-output '.isLast' jira-users-tmp.json )"
+  done="$( jq --raw-output '.isLast' jira-users-tmp.json )"
   if [[ "$done" = "true" ]] ; then
     break
   fi
 
   # Next URL is part of the output
-  URL="$( ./jq --raw-output '.nextPage' jira-users-tmp.json )"
+  URL="$( jq --raw-output '.nextPage' jira-users-tmp.json )"
 done
 
-echo "${ALL_USERS[@]}" | ./jq --slurp '.'
+echo "${ALL_USERS[@]}" | jq --slurp '.'
