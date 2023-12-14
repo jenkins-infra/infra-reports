42a5cc5138d28611290290ffb279c9a9cb907fdd
smerle33 <stephane.merle+jenkins@gmail.com>
2023-12-14 10:41:00 +0100

    wip

--- jenkins-infra-data/mirrorbits-mirrors-list.sh
+++ jenkins-infra-data/mirrorbits-mirrors-list.sh
@@ -3,4 +3,4 @@
 set -o nounset
 set -o errexit
 
-curl -s -L --max-redirs 2 -X GET "https://updates.jenkins.io/latest/jenkins.war?mirrorlist" | grep "jenkins/war" | xq -q "a" > mirrorbits-mirrors-list-raw.txt
+curl -s -L --max-redirs 2 -X GET "https://updates.jenkins.io/latest/jenkins.war?mirrorlist" | grep "jenkins/war" | xq -q "a"
