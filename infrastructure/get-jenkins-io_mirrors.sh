4615e0848c89cc2de0cb47c4867e4f3b4361a572
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-01-26 14:18:20 +0100

    readarray

--- infrastructure/get-jenkins-io_mirrors.sh
+++ infrastructure/get-jenkins-io_mirrors.sh
@@ -10,8 +10,9 @@ version=${VERSION:-v1}
 
 # Redirect to get.jenkins.io URL including last Jenkins version
 source="https://updates.jenkins.io/latest/jenkins.war?mirrorlist"
-mirrorRowXPath="//table/tbody/tr"
+lastUpdate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
+mirrorRowXPath="//table/tbody/tr"
 nameXPath="//td[2]"
 urlXPath="//td[3]"
 countryXPath="//td[4]"
@@ -19,25 +20,10 @@ continentXPath="//td[5]"
 
 mirrorRows=$(curl --silent --max-redirs 2 --request GET --location "${source}" | xq --node --xpath "${mirrorRowXPath}")
 
-names=()
-while read -r line; do
-   names+=("${line}")
-done <<< "$(echo "${mirrorRows}" | xq --xpath "${nameXPath}" || true)"
-
-urls=()
-while read -r line; do
-   urls+=("${line}")
-done <<< "$(echo "${mirrorRows}" | xq --xpath "${urlXPath}" || true)"
-
-countries=()
-while read -r line; do
-   countries+=("${line}")
-done <<< "$(echo "${mirrorRows}" | xq --xpath "${countryXPath}" || true)"
-
-continents=()
-while read -r line; do
-   continents+=("${line}")
-done <<< "$(echo "${mirrorRows}" | xq --xpath "${continentXPath}" || true)"
+readarray -t names <<< "$(echo "${mirrorRows}" | xq --xpath "${nameXPath}" || true)"
+readarray -t urls <<< "$(echo "${mirrorRows}" | xq --xpath "${urlXPath}" || true)"
+readarray -t countries <<< "$(echo "${mirrorRows}" | xq --xpath "${countryXPath}" || true)"
+readarray -t continents <<< "$(echo "${mirrorRows}" | xq --xpath "${continentXPath}" || true)"
 
 json='{"get.jenkins.io": {"mirrors": []}}'
 for ((i=0; i<${#names[@]}; i++)); do
@@ -49,9 +35,9 @@ for ((i=0; i<${#names[@]}; i++)); do
         '."get.jenkins.io".mirrors |= . + [{"name": $name, "url": $url, "country": $country, "continent": $continent}]')
 done
 
-# Add last update date and API current version
+# Add current date and API version
 json=$(echo "${json}" | jq \
-        --arg lastUpdate "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+        --arg lastUpdate "${lastUpdate}" \
         --arg version "${version}" \
         '. += {"lastUpdate": $lastUpdate, "version": $version}')
 
