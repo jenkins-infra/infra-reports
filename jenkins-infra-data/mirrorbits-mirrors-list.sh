3748b79cd81f295d0830ff3326874483630674cf
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2024-01-26 08:37:05 +0100

    output JSON

--- jenkins-infra-data/mirrorbits-mirrors-list.sh
+++ jenkins-infra-data/mirrorbits-mirrors-list.sh
@@ -3,4 +3,44 @@
 set -o nounset
 set -o errexit
 
-curl -s -L --max-redirs 2 -X GET "https://updates.jenkins.io/latest/jenkins.war?mirrorlist" | grep "jenkins/war" | xq -q "a"
+source="https://updates.jenkins.io/latest/jenkins.war?mirrorlist"
+mirrorRowXPath="//table/tbody/tr"
+
+nameXPath="//td[2]"
+urlXPath="//td[3]"
+countryXPath="//td[4]"
+continentXPath="//td[5]"
+
+mirrorRows=$(curl --silent --max-redirs 2 --request GET --location "${source}" | xq --node --xpath "${mirrorRowXPath}")
+
+names=()
+while read -r line; do
+   names+=("${line}")
+done <<< "$(echo "${mirrorRows}" | xq --xpath "${nameXPath}" || true)"
+
+urls=()
+while read -r line; do
+   urls+=("${line}")
+done <<< "$(echo "${mirrorRows}" | xq --xpath "${urlXPath}" || true)"
+
+countries=()
+while read -r line; do
+   countries+=("${line}")
+done <<< "$(echo "${mirrorRows}" | xq --xpath "${countryXPath}" || true)"
+
+continents=()
+while read -r line; do
+   continents+=("${line}")
+done <<< "$(echo "${mirrorRows}" | xq --xpath "${continentXPath}" || true)"
+
+json='{"mirrors": []}'
+for ((i=0; i<${#names[@]}; i++)); do
+    json=$(echo "${json}" | jq \
+        --arg rank "${i}" \
+        --arg name "${names[i]}" \
+        --arg url "${urls[i]}" \
+        --arg country "${countries[i]}" \
+        --arg continent "${continents[i]}" \
+        '.mirrors |= . + [{"rank": $rank, "name": $name, "url": $url, "country": $country, "continent": $continent}]')
+done
+echo "${json}"
