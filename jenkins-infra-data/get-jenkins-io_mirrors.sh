64121df6081ff03ddf065b4274ab4a504534b2fb
Damien Duportal <damien.duportal@gmail.com>
2024-06-20 09:02:18 +0200

    chore(jenkins-infra-data) make shell portable + add wrapper script to avoid data loss during partial updates (#68)
    * chore(jenkins-infra-data) make shell portable + add wrapper script to avoid data loss during partial updates
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * wip(jenkins-infra-data) only build this pipeline if changed or on main branch
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * wip: remove JQ 1.7 flags
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * fixup
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * fixup
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- jenkins-infra-data/get-jenkins-io_mirrors.sh
+++ jenkins-infra-data/get-jenkins-io_mirrors.sh
@@ -2,17 +2,17 @@
 
 set -o nounset
 set -o errexit
+set -o pipefail
 set -x
 
 command -v "jq" >/dev/null || { echo "[ERROR] no 'jq' command found."; exit 1; }
 command -v "xq" >/dev/null || { echo "[ERROR] no 'xq' command found."; exit 1; }
 command -v "dig" >/dev/null || { echo "[ERROR] no 'dig' command found."; exit 1; }
 
-version=${VERSION:-v1}
-reportName=${REPORT_NAME:-index.json}
-sourceHTML=${SOURCE_HTML:-source.html}
+test -d "${DIST_DIR}"
+
+sourceHTML="${DIST_DIR}"/source.html
 source="https://get.jenkins.io/index.html?mirrorstats"
-lastUpdate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 mirrorTableQuery="body > div > div > div > table"
 mirrorRowXPath="//table/tbody/tr"
@@ -23,40 +23,33 @@ fallback="archives.jenkins.io"
 curl --silent --max-redirs 2 --request GET --location "${source}" --output "${sourceHTML}"
 
 # Retrieving all rows of the table containing all mirrors
-mirrorRows=$(xq --node --query "${mirrorTableQuery}" "${sourceHTML}" | xq --node --xpath "${mirrorRowXPath}")
+mirrorRows="$(xq --node --query "${mirrorTableQuery}" "${sourceHTML}" | xq --node --xpath "${mirrorRowXPath}")"
 
-if [[ -z ${mirrorRows} ]]; then
+if [[ -z "${mirrorRows}" ]]; then
     echo "Error: no mirror returned from ${source}"
     exit 1
 fi
 
-# Keep only cells not finishing by " ago" (last update column) and that are not the fallback mirror
-readarray -t hostnames <<< "$(echo "${mirrorRows}" | xq --xpath "${cellXPath}" | grep -v ' ago' | grep -v "${fallback}" || true)"
+# Retrieve list of hostnames, one per line. Keep only cells not finishing by " ago" (last update column) and that are not the fallback mirror.
+hostnames="$(echo "${mirrorRows}" | xq --xpath "${cellXPath}" | grep -v ' ago' | grep -v "${fallback}")"
 
 json='{"mirrors": []}'
-for ((i=0; i<${#hostnames[@]}; i++)); do
+while IFS= read -r hostname
+do
     # As dig(1) can returns CNAME values, we need to filter IPs from its result(s) (those not finishing by a ".")
-    ipv4=$(dig +short "${hostnames[i]}" A | jq --raw-input --slurp 'split("\n") | map(select(test("\\.$") | not)) | map(select(length > 0))')
-    ipv6=$(dig +short "${hostnames[i]}" AAAA | jq --raw-input --slurp 'split("\n") | map(select(test("\\.$") | not)) | map(select(length > 0))')
-    json=$(echo "${json}" | jq \
-        --arg hostname "${hostnames[i]}" \
+    ipv4="$(dig +short "${hostname}" A | jq --raw-input --slurp 'split("\n") | map(select(test("\\.$") | not)) | map(select(length > 0))')"
+    ipv6="$(dig +short "${hostname}" AAAA | jq --raw-input --slurp 'split("\n") | map(select(test("\\.$") | not)) | map(select(length > 0))')"
+    json="$(echo "${json}" | jq \
+        --arg hostname "${hostname}" \
         --argjson ipv4 "${ipv4}" \
         --argjson ipv6 "${ipv6}" \
-        '.mirrors |= . + [{"hostname": $hostname, "ipv4": $ipv4, "ipv6": $ipv6}]')
-done
+        '.mirrors |= . + [{"hostname": $hostname, "ipv4": $ipv4, "ipv6": $ipv6}]')"
+done <<< "${hostnames}"
 
-if [[ ${i} -eq 0 ]]; then
+if [[ "${json}" == '{"mirrors": []}' ]]; then
     echo "Error: no mirror returned from ${source}"
     exit 1
 fi
 
-# Add current date and API version
-json=$(echo "${json}" | jq \
-        --arg lastUpdate "${lastUpdate}" \
-        --arg version "${version}" \
-        '. += {"lastUpdate": $lastUpdate, "version": $version}')
-
-# Update the "get.jenkins.io" section of the existing report before returning it
-result=$(jq --argjson json "${json}" '."get.jenkins.io" |=  $json' "${reportName}")
-
-echo "${result}" > "${reportName}"
+echo "${json}"
+exit 0
