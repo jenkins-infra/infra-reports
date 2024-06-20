6aed379231672adfbda89ed4ddc2f1861f97d729
Damien Duportal <damien.duportal@gmail.com>
2024-06-20 07:35:22 +0200

    chore(jenkins-infra-data) make shell portable + add wrapper script to avoid data loss during partial updates
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ jenkins-infra-data/generate-infra-data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# generate-infra-data.sh: Generate a JSON report named $1 in the directory $2 (with a optional version $3).
+#  Note: This script orchestrate generation by "parts": there are sub-scripts per services which generate partial content.
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -x
+
+REPORT_NAME="$1"
+test -n "${REPORT_NAME}"
+DIST_DIR="$2"
+test -n "${DIST_DIR}"
+mkdir -p "${DIST_DIR}"
+# Sub scripts need this
+export DIST_DIR
+VERSION="${3:-v1}"
+
+command -v "date" >/dev/null || { echo "[ERROR] no 'jq' command found."; exit 1; }
+command -v "jq" >/dev/null || { echo "[ERROR] no 'jq' command found."; exit 1; }
+
+json='{}'
+
+## get.jenkins.io
+# BSD Date and GNU Date have the same behavior with this pattern
+lastUpdate="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+getJenkinsIoData="$(./get-jenkins-io_mirrors.sh)"
+# Add current date and API version
+getJenkinsIoData="$(echo "${getJenkinsIoData}" | jq --raw-output0 --compact-output \
+  --arg lastUpdate "${lastUpdate}" \
+  --arg version "${VERSION}" \
+  '. += {"lastUpdate": $lastUpdate, "version": $version}')"
+echo "${json}"
+json="$(echo "${json}" | jq --raw-output0 --compact-output \
+  --argjson getJenkinsIoData "${getJenkinsIoData}" \
+  '. + {"get.jenkins.io": $getJenkinsIoData}' \
+)"
+
+## Write report
+echo "${json}" > "${DIST_DIR}/${REPORT_NAME}"
